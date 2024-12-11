88e50516cbbb6a26658b6f186f5d88c0f7582079
DNG222-code <dnagy20474@iesjoanramis.org>
2024-12-11 12:20:38 +0100

    Creación de archivo HolaMundo.java

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,21 @@
+package holamundo;
+
+import java.util.Scanner;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		
+		// Declaramos el Scanner.
+		Scanner scanner = new Scanner(System.in);
+		
+		// Pedimos al usuario un nombre.
+		System.out.print("Ingresa tu nombre: ");
+		String nombre = scanner.nextLine();
+		
+		// Monstramos el saludo y el nombre introducido.
+		System.out.println("¡Hola, " + nombre + "! Bienvenido a Java.");
+		
+	}
+
+}
